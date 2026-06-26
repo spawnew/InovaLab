361ed02ae82644a4cba7d81455693f03889b3c57
Lucas Sorzio <sorziolucas4@gmail.com>
2026-06-26 15:23:41 -0300

    mejora estilos

--- mi-app/src/Componentes/LectorPdf2/PanelHerramientas.jsx
+++ mi-app/src/Componentes/LectorPdf2/PanelHerramientas.jsx
@@ -20,9 +20,9 @@ export default function PanelHerramientas({
               <div className="form-panel">
         <div className="grupo-control">
           <h5>Tamaño de Letra</h5>
-            <div >
+            <div className="botones">
             <button className="btn btn-outline-secondary" onClick={() => setTamanioLetra(prev => Math.max(12, prev - 2))}>A-</button>
-            <span style={{ fontWeight: 'bold' }}>{tamanioLetra}px</span>
+            <p >{tamanioLetra}px</p>
             <button className="btn btn-outline-secondary" onClick={() => setTamanioLetra(prev => prev + 2)}>A+</button>
           </div>
         </div>
@@ -30,7 +30,7 @@ export default function PanelHerramientas({
        
         <div className="grupo-control">
           <h5>Herramientas de Voz</h5>
-          <div className="botones-voz">
+          <div className="botones">
             <button className="btn btn-success" onClick={alEscuchar}>Escuchar 🔊</button>
             <button className="btn btn-danger" onClick={alDetener}>Detener 🛑</button>
           </div>
@@ -39,7 +39,7 @@ export default function PanelHerramientas({
         {/* Sección de Temas */}
         <div className="grupo-control">
           <h5>Color de Fondo</h5>
-          <div className="botones-voz"> 
+          <div className="botones"> 
             <button onClick={() => aplicarTema('#ffffff', '#334155')} className="btn btn-light border text-start">☀️ Modo Claro</button>
             <button onClick={() => aplicarTema('#f4f1ea', '#433422')} className="btn text-start" style={{ backgroundColor: '#f4f1ea', color: '#433422', border: '1px solid #dcd7ca' }}>📜 Modo Sepia</button>
             <button onClick={() => aplicarTema('#1e293b', '#f8fafc')} className="btn btn-dark text-start">🌙 Modo Oscuro</button>
